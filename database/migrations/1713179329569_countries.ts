13eb149ddb11be402dc3e734463da39161e5c523
Antony Gitonga <antony@allenhark.com>
2024-04-15 23:02:21 +0300

    Updated country migrations

---
+++ database/migrations/1713179329569_countries.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'countries'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table.string('country_code')
+      table.boolean('synced').defaultTo(false)
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
